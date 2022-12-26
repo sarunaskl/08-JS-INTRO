c5a2f072bcb48c0668660331a53758384b51e466
sarunaskl <sarunas@klimavicius.org>
2022-12-26 19:42:48 +0100

    update

--- codeWars/multiplesOF3and5.js
+++
@@ -1,9 +0,0 @@
-function solution(number){
-    let all=0;
-      for ( let i = 1; i < number; i++) {
-        if (i % 3 === 0 || i % 5 === 0)  {
-          all = all + i;
-          }
-      }
-      return all;
-    }
